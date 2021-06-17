2edcbf9cd3e1f017edb174a36dfc483e4c7646c4
laiagc892 <laiagc892@gmail.com>
2021-06-17 12:47:33 +0200

    observables last update

--- 13-observables/src/app/home/home.component.ts
+++ 13-observables/src/app/home/home.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { interval, Observable, Subscription } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription
 
   constructor() { }
 
   ngOnInit() {
+    // Interval is a predefined observable that fires a number every x miliseconds you set on ().
+    // interval(1000) it's the observable here. So we're not storing the observable on firstObsSubscription, we store the subscription that subscribe returns.
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+
+    // Creating a custom Observable:
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater 3!'))
+        }
+        count++;
+      }, 1000);
+    });
+    
+    // Operators
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data =>{
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + data;
+    })).subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
+    });
+  }
+
+  ngOnDestroy() {
+    // Clear the subscription and prevent memory leaks, because we're not keeping old subscriptions around.
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
--- 13-observables/src/app/user/user.component.ts
+++ 13-observables/src/app/user/user.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import { UserService } from '../user.service';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -9,12 +11,18 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private userService: UserService) {
   }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
+  }
+
+  // When clicking the button
+  onActivate() {
+    // The activatedEmitter is the Subject from user.service
+    this.userService.activatedEmitter.next(true);
   }
 }
