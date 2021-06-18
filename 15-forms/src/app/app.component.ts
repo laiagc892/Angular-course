451e3a8929f0531be15fbe78b16baf3855c506c0
laiagc892 <laiagc892@gmail.com>
2021-06-18 09:35:17 +0200

    TD forms

--- 15-forms/src/app/app.component.ts
+++ 15-forms/src/app/app.component.ts
@@ -10,9 +10,36 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  genders = ['male', 'femlae', 'other'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // setValue() method overwrites the value of each control, so all values (of controls):
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'other'
+    // });
+
+    // patchValue() method applied on the form wrapped by ngForm ('signupForm'), you only rewrite specific certain controls, parts of the form you want:
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -20,6 +47,15 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    // console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    // Empty al the inputs and reset the state (valid, touched, ...)
+    this.signupForm.reset();
+    // We can also pass an object as in setValue() to reset(), which will then reset the form to these specific values.
   }
 }
