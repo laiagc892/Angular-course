6e245450735dffcd992386eff8690f886efbcf48
laiagc892 <laiagc892@gmail.com>
2021-06-21 17:17:04 +0200

    http update

--- 18-http-requests/src/app/app.component.ts
+++ 18-http-requests/src/app/app.component.ts
@@ -1,28 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+
+import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  loadedPosts = [];
+export class AppComponent implements OnInit, OnDestroy {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = null;
+  private errorSub: Subscription;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postService: PostsService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.errorSub = this.postService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    })
 
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
+    this.isFetching = true;
+    this.postService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error => {
+      this.isFetching = false;
+      this.error = error.message;
+      console.log(error);
+      // We will get that HTTP error response object by Angular and it will have an error key but the detailed content of what's in there depends on the API you're talking to.
+    });
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request:
+    this.postService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
+    this.isFetching = true;
+    this.postService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error => {
+      this.isFetching = false;
+      this.error = error.message;
+    });
   }
 
   onClearPosts() {
     // Send Http request
+    this.postService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
   }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
+  }
+
 }
+
+// SERVICES vs. COMPONENTS
+// Services are the parts in your Angular application that do the heavy lifting, the dirty work and your components are relatively lean, as lean as they can be and are mostly concerned with template related work.
+
+// SERVICES & COMPONENTS
+// The sending of the request and the transformation of the data, now lives in the service and this is a best practice when working with Angular and HTTP requests.
+
+// Service component SPLIT:
+// Move the part that is related to your template, which in my case here is managing the loading status and managing the loaded data, move that into the component and be informed about the result of your HTTP request by subscribing in the component but move the rest into the service and simply return the observable there so that you set up everything in the service but you can subscribe in the component.
---
+++ 18-http-requests/src/app/posts.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpParams, HttpEventType } from "@angular/common/http";
+import { map, catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+
+import { Post } from './post.model';
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+  error = new Subject<string>();
+
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = {title: title, content: content};  // The right side of the colon refers to the method arguments on "createAndStorePost".
+    
+    // Writting .json at the end is only a requirement by that Firebase REST API.
+    // Normally we send JSON data when interactin with a RESTful API and actually, that will happen here as well but the Angular HttpClient will take our Javascript object here and automatically convert it to JSON data for us.
+    this.http
+      .post<{ name: string }>(
+        'https://angular-course-49b92-default-rtdb.europe-west1.firebasedatabase.app/posts.json', 
+        postData,
+        {
+          observe: 'response'   // With observe, you can change the kind of data you get back (and not only get the body on the "responseData").
+        }
+      ).subscribe(responseData => {
+        console.log(responseData);
+      }, error => {
+        this.error.next(error.message);
+      });
+      // Requests are only sent when we subscribe: Angular (the HttpClient) will automatically extract the data attached to the response, so the response body and give you that. No subscription, no request!
+  }
+
+  fetchPosts() {
+    // To set multiple (query) params: variable search params, this object is immutable, so you actually need to overwrite it with a new, with the result of append.
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
+
+    // The get method is a so-called generic method. This means you can add angled brackets "<>" here and between the angled brackets, you store the type which this response will actually return as a body once it's done.
+    return this.http
+      .get<{ [key: string]: Post }>(
+        'https://angular-course-49b92-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+        {
+          headers: new HttpHeaders({ 'Custom-header': 'Hello' }),
+          // params: new HttpParams().set('print', 'pretty')  // To set just one query param key-pair.
+          params: searchParams  // Multiple query params.
+        }
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            // It is a good practice to wrap this with an "if" statement if you're using a for/in loop where you check if response data has key as its own property so that you're not trying to access the property of some prototype.
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });   // "..." is the Spread Operator.
+            }
+          }
+          return postsArray;
+        }),
+        catchError(errorRes => {
+          // Send to analytics server
+          return throwError(errorRes);   // Observable created by throw error.
+        })
+      );
+  }
+
+  deletePosts() {
+    return this.http.delete(
+      'https://angular-course-49b92-default-rtdb.europe-west1.firebasedatabase.app/posts.json', {
+        observe: 'events'
+      })
+      .pipe(
+        tap(event => {
+          // Tap operator won't interrupt the normal observable data flow, it just taps in there, allows you to do something but then automatically lets the response pass through.
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+            // ...
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        })
+      );
+  }
+
+}
